4bf9871c84961374f8b8a39ebf5a1a4d1487dc8f
juan2ramos <juan2ramos@gmail.com>
2014-11-04 12:01:50 -0500

    + Configuración plantilla + estructura header

---
+++ layout/default.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="es">
+<head>
+
+    <title><?php echo $title ?></title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="author" content="juan2ramos"/>
+    <meta name="contact" content="juan2ramos@gmail.com"/>
+    <meta name="description" content="Global Artistas"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+    <!-- Meta Facebook -->
+    <meta property="og:url" content="">
+    <meta property="og:title" content="Global Artistas"/>
+    <meta property="og:description" content=".."/>
+    <meta property="og:image" content="images/logo.png"/>
+
+
+    <link rel="shortcut icon" href="favicon.png">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic'
+          rel='stylesheet' type='text/css'>
+
+    <!-- Estilos -->
+    <script src="js/prefixfree.min.js"></script>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+<body>
+
+<header>
+    <figure>
+        <img src="" alt=""/>
+    </figure>
+    <form action="">
+        <input type="search"/>
+        <button><span class="icon-"></span></button>
+    </form>
+    <div class="header-top-rigth">
+        <span>Ingresar</span>
+        <button>Crear una cuenta</button>
+        <button><span class="icon-"></span></button>
+    </div>
+</header>
+<nav>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
+<div id="wrapper">
+<?php include $fileName; ?>
+</div>
+<footer ></footer>
+<!-- JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
