e20c8d0d3eb2641d60ff52b1232b328da437669c
Davidalr <david.cadc@gmail.com>
2014-11-05 09:53:27 -0500

    + ajustes nombres y informacion

--- views/global-artistas.php
+++ views/global-artistas.php
@@ -35,9 +35,9 @@
 
             <p>Canciones más sonadas</p>
             <ul class="song">
-                <li><span></span>La suegra</li>
-                <li><span></span>Pa las que sea</li>
-                <li><span></span>Lo del mercado</li>
+                <li><span></span>Letra chiquita</li>
+                <li><span></span>Aunque no sea Mayor</li>
+                <li><span></span>El ultimo Adios</li>
             </ul>
             <div class="links-song">
                 <span><a href="">Ver perfil</a></span>
@@ -54,10 +54,10 @@
             <img src="images/artistas/jimmy_gutierrez.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Jimmy Gutierrez</h4>
+            <h5>Pa las que sea</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -85,10 +85,10 @@
             <img src="images/artistas/jhon.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Jhon Alex Castaño</h4>
+            <h5>Dos Razones</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -97,9 +97,9 @@
 
             <p>Canciones más sonadas</p>
             <ul class="song">
-                <li><span></span>La suegra</li>
-                <li><span></span>Pa las que sea</li>
-                <li><span></span>Lo del mercado</li>
+                <li><span></span>Dos Razones</li>
+                <li><span></span>Amigos Con Derechos</li>
+                <li><span></span>Por Culpa Del Licor</li>
             </ul>
             <div class="links-song">
                 <span><a href="">Ver perfil</a></span>
@@ -116,10 +116,10 @@
             <img src="images/artistas/luis_posada.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Luis Alberto Posada</h4>
+            <h5>Sin un Adios</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -141,10 +141,10 @@
             <img src="images/artistas/charrito.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>El Charrito Negro</h4>
+            <h5>Te vas Arrepentir</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -166,10 +166,10 @@
             <img src="images/artistas/dario_gomez.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Dario Gomez</h4>
+            <h5>El Rey Del Despecho</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -191,10 +191,10 @@
             <img src="images/artistas/sebastian.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Sebastian El Coyote</h4>
+            <h5>Aunque Pasen Los Años</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -216,10 +216,10 @@
             <img src="images/artistas/ayala.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Giovanny Ayala</h4>
+            <h5>De Que Te Las picas</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Norteña</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -241,10 +241,10 @@
             <img src="images/artistas/jhonny_rivera.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Jhonny Rivera</h4>
+            <h5>Por Una Cerveza</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -266,10 +266,10 @@
             <img src="images/artistas/pipe.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Pipe Bueno</h4>
+            <h5>No Voy a Morir</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -291,10 +291,10 @@
             <img src="images/artistas/luisito.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Luisito Muñoz</h4>
+            <h5>Si Supieras</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Norteña</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -316,10 +316,10 @@
             <img src="images/artistas/fernando.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Fernando Burbano</h4>
+            <h5>Bohemio y Galan</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular </p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -341,10 +341,10 @@
             <img src="images/artistas/dario_dario.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Dario Dario</h4>
+            <h5>Muñeco De Vitrina</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -366,10 +366,10 @@
             <img src="images/artistas/rey_fonseca.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>El Rey Fonseca</h4>
+            <h5>La ultima flor</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Popular</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
@@ -391,10 +391,10 @@
             <img src="images/artistas/uriel.jpg" alt=""/>
         </figure>
         <div class="info-artist">
-            <h4>Gabriel Arriaga</h4>
-            <h5>El caballero de la ranchera</h5>
+            <h4>Uriel Henao</h4>
+            <h5>Prefiero Una Tumba En Colombia</h5>
 
-            <p><span>Genero: </span> Ranchera</p>
+            <p><span>Genero: </span> Norteña</p>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
             <span class="flaticon-star138"></span>
